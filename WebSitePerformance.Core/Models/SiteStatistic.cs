d3f7d45507b8852f87f32d63e1bf0a10e42eacef
Artem Artemenko <ArtemenkoArt82@gmail.com>
2019-10-26 01:03:13 +0300

    Add Core layer with models and SitemapParser

---
+++ WebSitePerformance.Core/Models/SiteStatistic.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebSitePerformance.Core.Models
+{
+    public class SiteStatistic
+    {
+        public int Id { get; set; }
+        public string SiteUrl { get; set; }
+        public DateTime TestDate { get; set; }
+        public string PageUrl { get; set; }
+        public long Performance { get; set; }
+        public bool ResponseError { get; set; }
+    }
+}
---
+++ WebSitePerformance.Web/Controllers/PerformanceController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebSitePerformance.Web.Controllers
+{
+    public class PerformanceController : Controller
+    {
+        // GET: Performance
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Performance/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Performance/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Performance/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Performance/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Performance/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Performance/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Performance/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
